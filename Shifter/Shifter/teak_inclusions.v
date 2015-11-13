13bc9c33307a8bc37fdf980b4151f6a105312b1b
Rob Taylor <rob.taylor@codethink.co.uk>
2015-11-13 11:16:28 +0000

    Remove lots of generated files and temp files

--- Shifter/Shifter/teak_inclusions.v
+++
@@ -1,4 +0,0 @@
-`include "/home/mahdi/Desktop/teak/runtime/verilog_main/runtime.v"
-`include "/home/mahdi/Desktop/teak/runtime/verilog_main/monitors.v"
-`include "/home/mahdi/Desktop/teak/runtime/verilog_main/example.v"
-`include "/home/mahdi/Desktop/teak/runtime/verilog_main/top.v"
--- Shifter/Shifter/view.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./teak --gui -e shifter.report -o dummy shifter.teak
