1e89b84ea987d85100ac0dea66d81f8114261c8b
Josh Bohde <josh@joshbohde.com>
2016-03-29 12:14:05 -0500

    Start of a Go -> Balsa pipeline.

--- src/Language/Transpile.hs
+++ src/Language/Transpile.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 -- |
 
 module Language.Transpile  (
@@ -5,78 +7,74 @@ module Language.Transpile  (
   transpileFile
   ) where
 
-import qualified Context                as C
-import           Language.Go.Parser     (goParse)
-import           Language.Go.Syntax.AST
-import qualified ParseTree              as PT
-import           Print                  (showTree)
-import qualified Report                 as R
+import           Control.Monad.Except    (MonadError, throwError, runExcept)
+import qualified Context                   as C
+import           Language.Go.Parser        (goParse)
+import           Language.SimpleGo.AST
+import           Language.SimpleGo.Process (compile)
+import qualified ParseTree                 as PT
+import           Print                     (showTree)
+import qualified Report                    as R
+import Data.Text (unpack)
+import qualified Data.Vector as U
 
 transpileFile f = do
   s <- readFile f
   let
     p = goParse f s
-  print p
   case p of
     Left s -> print s
-    Right t -> putStrLn $ showTree $ transpile t
+    Right t -> do
+      let p = runExcept $ do
+            simple <- compile t
+            transpile simple
+      case p of
+        Left s -> print s
+        Right tree -> putStrLn $ showTree tree
 
-transpile :: GoSource -> C.Context PT.Decl
-transpile = C.bindingsToContext1 . toBindings . getTopLevelDecl
+transpile :: MonadError String m => Program -> m (C.Context PT.Decl)
+transpile program = C.bindingsToContext1 <$> bindings
+  where
+    bindings = (toBindings . U.toList . declarations) program
 
-toBindings :: [GoDecl] -> [C.Binding PT.Decl]
-toBindings = zipWith (flip ($)) [(0 :: Int)..] . concatMap toBinding
+toBindings :: forall m. MonadError String m => [Declaration] -> m [C.Binding PT.Decl]
+toBindings = sequence . binds
+  where
+    binds :: [Declaration] -> [m (C.Binding PT.Decl)]
+    binds = zipWith toBinding [(0 :: Int)..]
 
-toExpr :: GoExpr -> PT.Expr
-toExpr (GoPrim prim) = PT.ValueExpr R.PosTopLevel PT.NoType (PT.IntValue 1)
-toExpr (Go1Op (GoOp op) e) = PT.UnExpr R.PosTopLevel PT.NoType (unOp op) (toExpr e)
-toExpr (Go2Op (GoOp op) e e') = PT.BinExpr R.PosTopLevel PT.NoType (binOp op) (toExpr e) (toExpr e')
 
-binOp :: String -> PT.BinOp
-binOp "*" = PT.BinMul
-binOp "/"  = PT.BinDiv
-binOp "?"  = PT.BinMod
-binOp "**"  = PT.BinPow
-binOp "+"  = PT.BinAdd
-binOp "-"  = PT.BinSub
-binOp "&&"  = PT.BinAnd
-binOp "||"  = PT.BinOr
-binOp "^"  = PT.BinXor
-binOp "<"  = PT.BinLT
-binOp ">"  = PT.BinGT
-binOp "<="  = PT.BinLE
-binOp ">="  = PT.BinGE
-binOp "!="  = PT.BinNE
-binOp "=="  = PT.BinEQ
-binOp _ = error "welp"
+toExpr :: MonadError String m => Expr -> m PT.Expr
+toExpr Zero = throwError "zero values are not supported"
+toExpr (Prim prim) = return $ PT.ValueExpr R.PosTopLevel PT.NoType (PT.IntValue 1)
+toExpr (UnOp Plus e) = PT.BinExpr R.PosTopLevel PT.NoType PT.BinAdd (PT.ValueExpr R.PosTopLevel PT.NoType (PT.IntValue 0)) <$> toExpr e
+toExpr (UnOp Minus e) = PT.BinExpr R.PosTopLevel PT.NoType PT.BinSub (PT.ValueExpr R.PosTopLevel PT.NoType (PT.IntValue 0)) <$> toExpr e
+toExpr (UnOp Not e) = PT.UnExpr R.PosTopLevel PT.NoType PT.UnNot <$> toExpr e
+toExpr (UnOp o _) = throwError $ "operator " ++ show o ++ " is not supported"
+toExpr (BinOp op e e') = PT.BinExpr R.PosTopLevel PT.NoType <$> binOp op <*> toExpr e <*> toExpr e'
 
-unOp "+" = error "unsupported operator"
-unOp "-" = PT.UnNeg
-unOp "!" = PT.UnNot
-unOp "^" = error "unsupported operator"
-unOp "*" = error "unsupported operator"
-unOp "&" = error "unsupported operator"
-unOp "<-"= error "unsupported operator"
-unOp _ = error "unrecognized operator"
+binOp :: MonadError String m => BinOp -> m PT.BinOp
+binOp Multiply = return PT.BinMul
+binOp Quotient = return PT.BinDiv
+binOp Remainder = return PT.BinMod
+binOp Add = return PT.BinAdd
+binOp Subtract = return PT.BinSub
+binOp BitwiseAnd = return PT.BinAnd
+binOp BitwiseOr  = return PT.BinOr
+binOp BitwiseXor = return PT.BinXor
+binOp LessThan = return PT.BinLT
+binOp GreaterThan = return PT.BinGT
+binOp LessThanEqual = return PT.BinLE
+binOp GreaterThanEqual = return PT.BinGE
+binOp NotEqual = return PT.BinNE
+binOp Equal  = return PT.BinEQ
+binOp o = throwError $ "operator " ++ show o ++ " is not supported"
 
-mayToExpr :: Maybe GoExpr -> PT.Expr
-mayToExpr Nothing = undefined
-mayToExpr (Just e) = toExpr e
-
-toBinding :: GoDecl -> [Int -> C.Binding PT.Decl]
-toBinding = bindings
+toBinding :: MonadError String m => Int -> Declaration -> m (C.Binding PT.Decl)
+toBinding i = bindings
   where
     namespace = C.OtherNamespace
-    bindings (GoConst cs) = cvSpecsToBindings toConst cs
-    bindings (GoVar cs) = cvSpecsToBindings id cs
-    bindings (GoType _) = [] -- cvTypeSpecToBindings
-    bindings (GoFunc (GoFuncDecl i s b)) = []
-    bindings (GoMeth _ ) = []
-    toConst :: PT.Expr -> PT.Expr
-    toConst = PT.ConstCheckExpr R.PosTopLevel
-    cvSpecsToBindings f cs = do
-      GoCVSpec ids typ exprs <- cs
-      let
-        exprs' = map Just exprs ++ repeat Nothing
-      (GoId name, e) <- zip ids exprs'
-      return $ \i -> C.Binding i name namespace R.Complete $ PT.ExprDecl R.PosTopLevel $ f $ mayToExpr e
+    bindings (Const (Id id) typ expr) = C.Binding i (unpack id) namespace R.Complete . PT.ExprDecl R.PosTopLevel <$> toExpr expr
+    bindings (Var (Id id) typ expr) = C.Binding i (unpack id) namespace R.Complete . PT.ExprDecl R.PosTopLevel <$> toExpr expr
+    bindings (Type (Id id) typ) = throwError "type declarations are not supported"
+    bindings (Func (Id id) sig block) = throwError "function declarations are not supported"
